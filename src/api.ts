cfeefde8b34603d4b4ba3cd996a673f4aa134ba9
vathana <siengvathana1@gmail.com>
2025-12-05 13:50:09 +0700

    g

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,32 @@
 import axios from "axios";
+import router from "./router/router.ts"; // make sure you have a router instance
+import AlertModal from "./components/AlertModal.vue";
+import { showAlert } from "./alertService.ts";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 const API = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // for httpOnly cookies
+  withCredentials: true, // for HttpOnly cookies
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// The browser will send httpOnly cookies automatically
+// Add a global response interceptor
+API.interceptors.response.use(
+  (response) => response, // just return successful responses
+  (error) => {
+    if (error.response?.status === 401) {
+      // Session expired
+      showAlert({
+        type: "error",
+        messageKey: "sessionExpired" 
+      });
+      router.push({ name: "Login" }); 
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
